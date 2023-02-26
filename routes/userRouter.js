1a958a8bebb00d9a85395541056cdae19564cc9a
Sajad_Adil <kinnkako72@gmail.com>
2023-02-26 22:03:30 +0300

    update user router&controller

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers, getUser, updateUser, deleteUser } = require('../controllers/userController');
-//const ROLES_LIST = require('../../config/roles_list');
-// const verifyRoles = require('../../middleware/verifyRoles');
 
 router.route('/')
     .get(getAllUsers)
@@ -14,11 +12,3 @@ router.route('/:id')
     .get(getUser);
 
 module.exports = router;
-
-
-// router.route('/')
-//     .get(verifyRoles(ROLES_LIST.Admin), usersController.getAllUsers)
-//     .delete(verifyRoles(ROLES_LIST.Admin), usersController.deleteUser);
-
-// router.route('/:id')
-//     .get(verifyRoles(ROLES_LIST.Admin), usersController.getUser);

e3fb9c5cacb67c3358069bcb4d76d98f2730dcdb
Sajad_Adil <kinnkako72@gmail.com>
2023-02-26 22:01:56 +0300

    update user router&controller

---
+++ controllers/userController.js
@@ -0,0 +1,49 @@
+const User = require('../models/user');
+
+const getAllUsers = async (req, res) => {
+    const users = await User.find();
+    if (!users) return res.status(204).json({ 'message': 'No users found' });
+    res.json(users);
+}
+
+const getUser = async (req, res) => {
+    if (!req?.params?.id) return res.status(400).json({ "message": 'User ID required' });
+    const user = await User.findOne({ _id: req.params.id }).exec();
+    if (!user) {
+        return res.status(204).json({ 'message': `User ID ${req.params.id} not found` });
+    }
+    res.json(user);
+}
+
+const updateUser = async (req, res) => {
+    if (!req?.body?.id) {
+        return res.status(400).json({ 'message': 'ID parameter is required.' });
+    }
+
+    const user = await User.findOne({ _id: req.body.id }).exec();
+    if (!user) {
+        return res.status(204).json({ "message": `No user matches ID ${req.body.id}.` });
+    }
+    if (req.body?.firstname) user.firstname = req.body.firstname;
+    if (req.body?.lastname) user.lastname = req.body.lastname;
+    const result = await user.save();
+    res.json(result);
+}
+const deleteUser = async (req, res) => {
+    if (!req?.body?.id) return res.status(400).json({ "message": 'User ID required' });
+    const user = await User.findOne({ _id: req.body.id }).exec();
+    if (!user) {
+        return res.status(204).json({ 'message': `User ID ${req.body.id} not found` });
+    }
+    const result = await user.deleteOne({ _id: req.body.id });
+    res.json(result);
+}
+
+module.exports = {
+    getAllUsers,
+    getUser,
+    updateUser,
+    deleteUser
+}
+
+
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,42 +1,24 @@
-const express = require("express")
-const router = express.Router()
-const validateObjectId = require("../middlewares/validateObjectid");
-const bcrypt = require("bcrypt");
-const _ = require("lodash");
-const { User, validate } = require("../models/user");
+const express = require('express');
+const router = express.Router();
+const { getAllUsers, getUser, updateUser, deleteUser } = require('../controllers/userController');
+//const ROLES_LIST = require('../../config/roles_list');
+// const verifyRoles = require('../../middleware/verifyRoles');
+
+router.route('/')
+    .get(getAllUsers)
+    .delete(deleteUser)
+    .put(updateUser);
 
 
-router.get("/", async (req, res) => {
-    const users = await User.find()
-    res.send(users)
-    try {
-    const users = await User.find();
-    res.json(users);
-} catch (err) {
-    res.status(500).json({ message: err.message });
-}
-    
-})
-
-router.get("/:id", validateObjectId, async (req, res) => {
-    const user = await User.findById(req.params.id);
-    if (!user)
-        return res.status(404).send("The user with the given ID was not found.");
-
-    res.send(user);
-});
-
-router.post("/", async (req, res) => {
-
-});
-
-router.put("/:id",async (req, res) => { 
-
-});
-
-router.delete("/:id", async (req, res) => { 
-
-});
-
+router.route('/:id')
+    .get(getUser);
 
 module.exports = router;
+
+
+// router.route('/')
+//     .get(verifyRoles(ROLES_LIST.Admin), usersController.getAllUsers)
+//     .delete(verifyRoles(ROLES_LIST.Admin), usersController.deleteUser);
+
+// router.route('/:id')
+//     .get(verifyRoles(ROLES_LIST.Admin), usersController.getUser);

c422eef52b53c4ad588c5fc4e0cde63b190e0b0e
Sajad_Adil <kinnkako72@gmail.com>
2023-02-26 21:51:43 +0300

    added authentication

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const userRouter= require('./routes/userRouter')
 const connectDB = require('./config/dbConn')
 const registerRouter = require('./routes/registerRouter');
-const { request } = require('express');
+const authRouter = require('./routes/authRouter');
 const verifyJWT = require('./middlewares/verifyJWT');
-const refreshToken = require('./routes/refresh')
+const refreshToken = require('./routes/refreshRouter')
 const cookieParser = require('cookie-parser');
-
+const userRouter = require('./routes/userRouter')
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/register", registerRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/refresh",refreshToken );
+
+app.use(verifyJWT);
+app.use("/api/user", userRouter);
 
 
 connectDB();
@@ -19,4 +24,5 @@ connectDB();
 app.listen(process.env.PORT || 5000, () => {
         console.log('app is listening on port', process.env.PORT || 5000)
 })
+
 

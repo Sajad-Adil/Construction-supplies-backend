74a2beaf0691bbc1b3a950f7c280a8f324b69880
Sajad_Adil <kinnkako72@gmail.com>
2023-03-08 15:53:04 +0300

    new update

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/',
-        );
+     try {
+        await mongoose.connect(process.env.DATABASE_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
     } catch (err) {
         console.error(err);
     }
 }
 module.exports = connectDB;
-

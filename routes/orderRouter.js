764750f44903f4371f16d3e544bd76e9b024a234
Sajad_Adil <kinnkako72@gmail.com>
2023-03-11 18:57:08 +0300

    update orders router

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -6,10 +6,14 @@ const verifyRoles = require('../middlewares/verifyRoles');
 const verifyJWT = require('../middlewares/verifyJWT')
 
 router.route('/')
-    .get(verifyJWT,verifyRoles(ROLES_LIST.Master),getOrdersByStore)
     .post(verifyJWT,verifyRoles(ROLES_LIST.User),createOrder)
     .delete(verifyJWT,verifyRoles(ROLES_LIST.User),deleteOrder)
 
+
+router.route('/ordersForStore/:id')
+    .get(verifyJWT,verifyRoles(ROLES_LIST.Master, ROLES_LIST.Admin),getOrdersByStore)
+
+    
 router.route('/:id')
     .get(verifyJWT, getOrder);
 
